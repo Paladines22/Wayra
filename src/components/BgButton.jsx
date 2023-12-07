e8f61f4e43ca4957d873e42cbdaac1f8e098a173
ValentinaPaladines <val.paladinezunal@gmail.com>
2023-12-06 20:20:05 -0500

    bg button

--- src/components/BgButton.jsx
+++ src/components/BgButton.jsx
@@ -1,11 +1,11 @@
 function bgButton({ text }) {
   return (
     <div className="mx-auto">
-      <a href="https://Wa.me/3157317717" target="_blank">
-        <button className="hover:opacity-70 transition duration-500 bg-[#111112]/30 border border-2 text-2xl lg:text-3xl border w-50  cursor-pointer flex gap-4 ">
+      <button className="hover:opacity-70 transition duration-500 bg-[#111112]/30 border border-2 text-2xl lg:text-3xl border w-50  cursor-pointer flex gap-4 ">
+        <a href="https://Wa.me/3157317717" target="_blank">
           <p className="font-bold my-4 mx-auto text-white">{text}</p>
-        </button>
-      </a>
+        </a>
+      </button>
     </div>
   );
 }
